101018d151cfdc9408b74ecead9dedb62148f297
roopehub <roope.heinonen94@gmail.com>
2019-07-31 22:16:19 +0300

    added road to beer / hsl integration to show route

--- src/Components/PopUp/CustomPopup.js
+++ src/Components/PopUp/CustomPopup.js
@@ -1,31 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Popup } from 'react-leaflet';
+import { createReittiopasUrl } from '../../Controls/LocateControl/LocateControl';
+import Button from '@material-ui/core/Button';
 
-const customPopup = (props) => {
 
-    const createReittiopasUrl = (from, dest) => {
-        const today = new Date();
-        const hour = today.getHours();
-        const minutes = today.getMinutes();
-
-        let url = "http://www.reittiopas.fi/fi/?"+ from +"&to="+ dest +",helsinki&hour="+ hour +"&minute="+ minutes +"";
-        console.log(url);
-        return url;
+class customPopup extends Component {
+    
+    render() {
+        let price = String(this.props.content.beer_price).split('');
+        return (
+            <Popup>
+                <div>
+                    <h2>{this.props.content.name}</h2>
+                    <hr/>
+                    <p>Beer: {this.props.content.beer_name}</p>
+                    <p>Price: <strong>{price[0]}.{price[1]}</strong> €</p>
+                    <Button variant="outlined" onClick={() => createReittiopasUrl(this.props)}>Road to Beer! <i className="material-icons">tram</i> </Button>
+                </div>
+            </Popup>
+        )
     }
-
-    let price = String(props.content.beer_price).split('');
-
-    return (
-        <Popup>
-            <div>
-                <h2>{props.content.name}</h2>
-                <hr/>
-                <p>Beer: {props.content.beer_name}</p>
-                <p>Price: <strong>{price[0]}.{price[1]}</strong> €</p>
-                <a href={() => this.createReittiopasUrl(null, props.content.address)}><button>Hae reitti:</button></a>
-            </div>
-        </Popup>
-    )
 }
 
 export default customPopup;
---
+++ src/Controls/LocateControl/LocateControl.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import L from 'leaflet';
+
+export const createReittiopasUrl = (props) => {
+    const today = new Date();
+    const hour = today.getHours();
+    const minutes = today.getMinutes();
+
+    let dest = props.content.address ? props.content.address.replace(/\s/g, "") : null;
+
+    if (dest && dest !== null) {
+        navigator.geolocation.getCurrentPosition((location) => {
+            let latLng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+            axios.get('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + latLng.lat + '&lon=' + latLng.lng + '')
+                .then((resp) => {
+                    let address = resp.data.display_name.split(",");
+                    let from = ([].concat(address[0], address[1], address[2])).join("").replace(/\s/g, "");
+                    let url = "http://www.reittiopas.fi/fi/?from=" + from + "&to=" + dest + ",helsinki&hour=" + hour + "&minute=" + minutes + "";
+                    console.log(url);
+                    window.open(url, "_blank");
+                });
+        },
+        error => {
+            console.log(error);
+            alert("Getting location failed");
+        })
+    }
+}
