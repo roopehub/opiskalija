42081a5a505f227a48a1911c8566c52394d63caf
roopehub <roope.heinonen94@gmail.com>
2019-08-02 04:48:40 +0300

    added some styles to popup and other drinks

--- src/Components/CustomIcon/CustomIcon.js
+++ src/Components/CustomIcon/CustomIcon.js
@@ -1,4 +1,5 @@
 import L from 'leaflet';
+import classes from './CustomIcon.css';
 
     export const BIG = new L.Icon({
         iconUrl: require('../../Assets/Images/beer_icon_green.png'),
@@ -8,8 +9,8 @@ import L from 'leaflet';
         shadowUrl: null,
         shadowSize: null,
         shadowAnchor: null,
-        iconSize: new L.Point(35, 45),
-        className: 'CustomMarker'
+        iconSize: new L.Point(40, 50),
+        className: classes.CustomMarker
     });
     
     export const BIY = new L.Icon({
@@ -18,7 +19,7 @@ import L from 'leaflet';
         iconAnchor: [0, 0],
         popupAnchor: [17, 10],
         iconSize: new L.Point(35, 45),
-        className: 'CustomMarker'
+        className: classes.CustomMarker
     });
     
     export const BIO = new L.Icon({
@@ -27,7 +28,7 @@ import L from 'leaflet';
         iconAnchor: [0, 0],
         popupAnchor: [17, 10],
         iconSize: new L.Point(35, 45),
-        className: 'CustomMarker'
+        className: classes.CustomMarker
     });
     
     export const BIR = new L.Icon({
@@ -36,7 +37,7 @@ import L from 'leaflet';
         iconAnchor: [0, 0],
         popupAnchor: [17, 10],
         iconSize: new L.Point(35, 45),
-        className: 'CustomMarker'
+        className: classes.CustomMarker
     });
 
 
--- src/Components/PopUp/CustomPopup.js
+++ src/Components/PopUp/CustomPopup.js
@@ -4,19 +4,63 @@ import { Popup } from 'react-leaflet';
 import { createReittiopasUrl } from '../../Controls/LocateControl/LocateControl';
 import Button from '@material-ui/core/Button';
 
-
 class customPopup extends Component {
     
     render() {
-        let price = String(this.props.content.beer_price).split('');
+
+        let beerPrice = null;
+        let ciderPrice = null;
+        let longdrinkPrice = null;
+
+        if (this.props.content.beer_price) beerPrice = String(this.props.content.beer_price).split('');
+        if (this.props.content.cider_price) ciderPrice = String(this.props.content.cider_price).split('');
+        if (this.props.content.longdrink_price) ciderPrice = String(this.props.content.longdrink_price).split('');
+        
+        let pictureStyles = {
+            backgroundImage: 'url(' + this.props.content.photo + ')',
+            width: '25vh',
+            height: '20vh',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            boxShadow: '1px 2px 3px',
+            marginBottom: '20px'
+        }
+
         return (
             <Popup className={classes.Popup}>
                 <div>
                     <h2>{this.props.content.name}</h2>
-                    <hr/>
-                    <p>Beer: {this.props.content.beer_name}</p>
-                    <p>Price: <strong>{price[0]}.{price[1]}</strong> €</p>
-                    <Button variant="outlined" onClick={() => createReittiopasUrl(this.props)}>Road to Beer! <i className="material-icons">tram</i> </Button>
+                    { 
+                      this.props.content.photo ? <div style={pictureStyles}></div> : null
+                    }
+                    <div className={classes.DrinkDiv}>
+                        <p>{this.props.content.beer_name}</p>
+                        <p>€ = <strong>{beerPrice[0]}.{beerPrice[1]}</strong></p>
+                    </div>
+                    <br />
+                    { 
+                        this.props.content.cider_name && ciderPrice ? ( 
+                        <div className={classes.DrinkDiv}>
+                            <p>Cider: {this.props.content.cider_name}</p>
+                            <p>€ = <strong>{ciderPrice[0]}.{ciderPrice[1]}</strong></p>
+                        </div>
+                         ) : null
+                    }
+                    { 
+                        this.props.content.longdrink_name && longdrinkPrice ? (
+                        <div className={classes.DrinkDiv}>
+                            <p>Longdrink: {this.props.content.longdrink_name}</p>
+                            <p>€ = <strong>{longdrinkPrice[0]}.{longdrinkPrice[1]}</strong></p>
+                        </div>) : null
+                    }
+
+                    <div className={classes.CenteredDiv}>
+                        <Button variant="outlined" onClick={() => createReittiopasUrl(this.props)}>Road to Beer! <i className="material-icons">tram</i> </Button>
+                    </div>
+                    <p style={{textAlign: 'center', margin: '0'}}>--- or ---</p>
+                    <div className={classes.CenteredDiv}>
+                        <Button variant="outlined"> Get citybike <i className="material-icons">directions_bike</i> </Button>
+                    </div>
                 </div>
             </Popup>
         )
